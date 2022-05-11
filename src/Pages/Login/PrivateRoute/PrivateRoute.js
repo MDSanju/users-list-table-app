557db6e3ae0f31d54ef5c3e60bfd2746d7006417
MD Sanju <mdsanju387@gmail.com>
2022-05-12 01:02:26 +0600

    Private Route Created

---
+++ src/Pages/Login/PrivateRoute/PrivateRoute.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
+import { ScaleLoader } from "react-spinners";
+import useAuth from "../../../hooks/useAuth";
+
+const PrivateRoute = ({ children, ...rest }) => {
+  const { user, isLoading } = useAuth();
+  let location = useLocation();
+
+  if (isLoading) {
+    return (
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "30vh",
+        }}
+      >
+        <ScaleLoader color={"#E2DED0"} size={85} />
+      </div>
+    );
+  }
+
+  if (user.email) {
+    return children;
+  }
+
+  return <Navigate to="/login" state={{ from: location }} />;
+};
+
+export default PrivateRoute;
