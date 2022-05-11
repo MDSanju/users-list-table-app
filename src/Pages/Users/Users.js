1d2fb00b573688e2026071e44f140b077a39c6f5
MD Sanju <mdsanju387@gmail.com>
2022-05-12 05:29:12 +0600

    fetch users data

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { UsersPage, UsersTitle } from "../styles/Users.styles";
 
 const Users = () => {
+  const [users, setUsers] = useState([]);
+  console.log(users);
+
+  useEffect(() => {
+    fetch(
+      "https://datapeace-storage.s3-us-west-2.amazonaws.com/dummy_data/users.json"
+    )
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
   return (
-    <div>
-      <h2>Users</h2>
-    </div>
+    <UsersPage>
+      <UsersTitle>Users</UsersTitle>
+      <h2>All users: {users.length}!</h2>
+    </UsersPage>
   );
 };
 
---
+++ src/Pages/styles/Users.styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const UsersPage = styled.div`
+  padding: 35px;
+`;
+
+export const UsersTitle = styled.h2`
+  font-size: 42px;
+  font-weight: 500;
+`;
