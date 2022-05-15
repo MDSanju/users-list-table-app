b69465c5f2c4d4332421cc775950084eadd6a107
MD Sanju <mdsanju387@gmail.com>
2022-05-15 17:33:01 +0600

    routing part done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import AuthProvider from "./contexts/AuthProvider/AuthProvider";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Users from "./Pages/Users/Users";
+import PrivateRoute from "./Pages/Login/PrivateRoute/PrivateRoute";
+import Login from "./Pages/Login/Login/Login";
+import Registration from "./Pages/Login/Registration/Registration";
 import UserDetails from "./Pages/UserDetails/UserDetails";
 import NoMatch from "./Pages/NoMatch/NoMatch";
 import "./App.css";
@@ -14,7 +17,16 @@ function App() {
           <Routes>
             <Route path="/" element={<Users />} />
             <Route path="/users" element={<Users />} />
-            <Route path="/user/:userId" element={<UserDetails />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route
+              path="/user/:userId"
+              element={
+                <PrivateRoute>
+                  <UserDetails />
+                </PrivateRoute>
+              }
+            />
             <Route path="*" element={<NoMatch />} />
           </Routes>
         </Router>
